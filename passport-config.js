ade3ddae5aa78dc1ba46d9ced04e8c6810bb3133
austynhorne <82992053+austynhorne@users.noreply.github.com>
2025-11-01 16:06:56 -0600

    Implement Google OAuth user creation and cleanup unneeded code

--- passport-config.js
+++ passport-config.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('./models/User');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -8,12 +9,35 @@ passport.use(new GoogleStrategy({
         ? 'https://cse341-week5project.onrender.com/auth/google/callback'
         : 'http://localhost:8080/auth/google/callback'
   },
-  (accessToken, refreshToken, profile, done) => {
-    // Here you would find/create user in DB
-    done(null, profile);
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      // Find or create user in DB
+      let user = await User.findOne({ oauthId: profile.id, oauthProvider: 'google' });
+      if (!user) {
+        user = new User({
+          username: profile.displayName,
+          email: profile.emails[0].value,
+          oauthProvider: 'google',
+          oauthId: profile.id,
+          profilePic: profile.photos[0].value
+        });
+        await user.save();
+      }
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
   }
 ));
 
 // Serialize user into session
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((user, done) => done(null, user));
+passport.serializeUser((user, done) => done(null, user._id));
+// Deserialize user from session
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
